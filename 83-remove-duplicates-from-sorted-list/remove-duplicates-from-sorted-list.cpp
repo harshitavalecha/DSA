acb1bd07603c5ba3b1aec875e8847c7c644e6b39
Harshita Valecha <csh23060@glbitm.ac.in>
2026-02-05 11:35:15 +0530

    Time: 4 ms (3.06%) | Memory: 16.3 MB (9.90%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -11,19 +11,34 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head == NULL) return NULL;
-         ListNode*curr=head;
-         while(curr != NULL)
-         {
-            if((curr->next != NULL) && (curr->val == curr->next->val)){
-           ListNode*nextpointer=curr->next->next;
-            ListNode*nodetodelete=curr->next;
-            delete(nodetodelete);
-            curr->next=nextpointer;
-            }
-            else 
-            curr=curr->next;
-         }
-         return head;
+    //     if(head == NULL) return NULL;
+    //      ListNode*curr=head;
+    //      while(curr != NULL)
+    //      {
+    //         if((curr->next != NULL) && (curr->val == curr->next->val)){
+    //        ListNode*nextpointer=curr->next->next;
+    //         ListNode*nodetodelete=curr->next;
+    //         delete(nodetodelete);
+    //         curr->next=nextpointer;
+    //         }
+    //         else 
+    //         curr=curr->next;
+    //      }
+    //      return head;
+    // }
+    if(head==NULL) return NULL;
+    ListNode*curr=head;
+    while(curr!= NULL)
+    {
+        if((curr->next !=NULL) &&(curr->val == curr->next->val))
+        {
+            ListNode*nextpointer= curr->next->next;
+            ListNode*nodetodelete= curr->next;
+            delete (nodetodelete);
+             curr->next=nextpointer;
+        }
+        else curr=curr->next;
+    }
+   return head;
     }
 };
