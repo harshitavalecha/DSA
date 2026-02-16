5a801f892eaf2288160458097afee1ff6ad94ef7
Harshita Valecha <csh23060@glbitm.ac.in>
2026-02-16 17:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (51.68%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        stack<char>st;
-        for(int i=0;i<s.size(); i++){
-            st.push(s[i]);
-        }
-        int i=0;
-        while(!st.empty()){
-            s[i]=st.top();
-            i++;
-            st.pop();
+        // stack<char>st;
+        // for(int i=0;i<s.size(); i++){
+        //     st.push(s[i]);
+        // }
+        // int i=0;
+        // while(!st.empty()){
+        //     s[i]=st.top();
+        //     i++;
+        //     st.pop();
+        // }
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right){
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
     }
 };
