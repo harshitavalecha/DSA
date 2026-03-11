0b1b5e2bcbab6736bd4c1eb55a947c27ee6b6413
Harshita Valecha <csh23060@glbitm.ac.in>
2026-03-11 22:03:11 +0530

    Time: 4 ms (20.14%) | Memory: 28.3 MB (7.93%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+    //     map<int,int>mp;
+    //     for(int i=0;i<nums.size();i++){
+    //         mp[nums[i]]++;
+    //     }
+    //    for(auto it:mp){
+    //     if(it.second> (nums.size()/2)) return it.first;
+    //    }
+    //    return -1;
+
+
+    // second method 
+    int candidate;
+    int count=0;
+    for(int i=0;i<nums.size();i++){
+        if(count==0){
+            count=1;
+            candidate=nums[i];
+
         }
-       for(auto it:mp){
-        if(it.second> (nums.size()/2)) return it.first;
-       }
-       return -1;
+        else{
+            if(nums[i]==candidate) count++;
+            else count--;
+        }
+    }
+    return candidate;
     }
 };
