805f8becaf262464ee45b5b7bca95943fa68e24a
Harshita Valecha <csh23060@glbitm.ac.in>
2026-03-12 19:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (68.09%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -4,18 +4,21 @@ public:
         int index=-1;
         int n=nums.size();
         for(int i=n-2;i>=0;i--){
-            if(nums[i+1]>nums[i]){
+            if(nums[i]<nums[i+1]){
                 index=i;
                 break;
             }
         }
-        if(index==-1) {reverse(nums.begin(),nums.end());
-        return;}
+        if(index==-1) {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
         for(int i=n-1;i>index;i--){
             if(nums[i]>nums[index]){
                 swap(nums[i],nums[index]);
                 break;
             }
+           
         }
         reverse(nums.begin()+index+1,nums.end());
     }
