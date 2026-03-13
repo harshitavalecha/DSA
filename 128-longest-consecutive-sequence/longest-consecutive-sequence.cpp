18ad9f250d31802240d388f7616833f56beddbad
Harshita Valecha <csh23060@glbitm.ac.in>
2026-03-13 11:14:58 +0530

    Time: 86 ms (36.16%) | Memory: 89.1 MB (23.68%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-      int lastlargest=INT_MIN;
-      int count=0;
-      int longest=1;
-      int n=nums.size();
-      sort(nums.begin(),nums.end());
-      if(n==0) return 0;
-      for(int i=0;i<n;i++)  {
-          if(nums[i]-1 == lastlargest){
+    //   int lastlargest=INT_MIN;
+    //   int count=0;
+    //   int longest=1;
+    //   int n=nums.size();
+    //   sort(nums.begin(),nums.end());
+    //   if(n==0) return 0;
+    //   for(int i=0;i<n;i++)  {
+    //       if(nums[i]-1 == lastlargest){
+    //         count+=1;
+    //         lastlargest=nums[i];
+    //       }
+    //    else if(nums[i]==lastlargest){
+    //     continue;
+    //    }
+    //    else {
+    //     count=1;
+    //     lastlargest=nums[i];
+    //    }
+    //    longest=max(longest,count);
+    //   }
+    // //   longest=max(longest,count);
+    //   return longest;
+
+
+    // optimized 
+    int n=nums.size();
+  
+    int longest=1;
+    if(n==0) return 0;
+    unordered_set<int> st;
+    for(int i=0;i<n;i++){
+        st.insert(nums[i]);
+    }
+    for(auto it:st)
+    {
+        if(st.find(it-1)==st.end())
+        {
+            int count=1;
+            int x=it;
+            while(st.find(x+1)!=st.end()){
+            x+=1;
             count+=1;
-            lastlargest=nums[i];
-          }
-       else if(nums[i]==lastlargest){
-        continue;
-       }
-       else {
-        count=1;
-        lastlargest=nums[i];
-       }
-       longest=max(longest,count);
-      }
-    //   longest=max(longest,count);
+        }
+         longest= max(longest,count);
+        }
+        // longest= max (longest,count);
+    }
+    
       return longest;
     }
+    
 };
